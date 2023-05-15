e984508b2f942dfd104f385e987a6e59d1831a34
Emon <emon.mhk69@gmail.com>
2023-05-15 22:32:14 +0600

    pagination done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -27,11 +27,30 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const productCollection = client.db("emajohnDB").collection("products");
+    const productCollection = client.db("emaJohnDB").collection("products");
 
 
     app.get("/products", async(req,res)=>{
-      const result = await productCollection.find().toArray();
+
+      const page = parseInt(req.query.page) || 0;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = page * limit;
+
+      const result = await productCollection.find().skip(skip).limit(limit).toArray();
+      res.send(result);
+
+    });
+
+    app.get("/totalProducts", async(req,res)=>{
+      const result = await productCollection.estimatedDocumentCount();
+      res.send({totalProducts: result});
+    });
+
+    app.post("/productsByIds", async (req,res)=>{
+      const ids = req.body;
+      const objectIds = ids.map(id=> new ObjectId(id));
+      const query = { _id: {$in: objectIds} }
+      const result = await productCollection.find(query).toArray();
       res.send(result);
     })
 
